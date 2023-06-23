3c80d72a53c615687a17831d6275d7a52b58a469
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-06-23 22:02:47 +0300

    React-Formik-Yup
    React içerisinde formik ve yup kütüphaneleri kullanılarak incelendi.

---
+++ React-Formik/react-formikapp/src/App.jsx
@@ -0,0 +1,13 @@
+import FormikForm from "./FormikForm"
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <FormikForm />
+    </>
+  )
+}
+
+export default App
---
+++ React-Formik/react-formikapp/src/schema.js
@@ -0,0 +1,27 @@
+import * as yup from 'yup';
+
+export const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Geçerli bir email giriniz")
+    .required("Email girmek zorunludur"),
+  age: yup
+    .number()
+    .positive("Lütfen pozitif bir yaş giriniz")
+    .integer("Lütfen yaşınızı tam sayı olarak giriniz")
+    .required("Yaş girmek zorunludur"),
+  password: yup
+    .string()
+    .min(5, "Lütfen minumun 5 karakter giriniz")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+      message: "Lütfen en az 1 büyük harf, 1 küçük harf ve 1 sayı giriniz",
+    })
+    .required("Şifre girmek zorunludur"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Şifreler eşleşmiyor")
+    .required("Tekrar şifre girmek zorunludur"),
+});
+
+
+  export default schema;
